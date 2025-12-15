54cf278219ce99b5804a6f90fb7bd693e9adea76
Neoibnqayyimrx <Neoibnqayyim@hotmail.com>
2025-12-15 13:34:43 +0100

    next.config update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
